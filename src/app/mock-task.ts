a0c6aae75f55171d704ab2b7159210883e2f00ad
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2022-08-12 14:02:40 -0300

    task

---
+++ src/app/mock-task.ts
@@ -0,0 +1,26 @@
+import { Task } from "./task"
+
+export const tasks:Task[] = [
+    {
+        id:1,
+        text:"Finish laundry",
+        day: '12/08/22',
+        time: '17:00',
+        reminder:false
+    },
+    {
+        id:2,
+        text:"Go to the gym",
+        day: '12/08/22',
+        time: '10:00',
+        reminder:true
+    },
+    {
+        id:3,
+        text:"Stretching",
+        day: '12/08/22',
+        time: '23:00',
+        reminder:true
+    },
+
+]
